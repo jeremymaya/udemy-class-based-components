557b77d1d2dfd2cc92b991bfb52ae2e7f1eca094
Kyungrae Kim <kyungraekim@outlook.com>
2020-11-04 10:52:14 -0800

    determining season and ternary expression in JSX complete

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -9,12 +9,15 @@ const getSeason = (lat, month) => {
     }
 }
 
-const SeasonDisplay = (props) => {
+const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    
-    console.log(season);
+    const text = season === 'winter' ? 'Burr, it is chilly!' : 'Let\'s hit the beach'
 
-    return <div>Season Display</div>
+    return (
+        <div>
+            <h1>{text}</h1>
+        </div>
+    )
 };
 
 export default SeasonDisplay;
